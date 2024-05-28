b18d3de09b1e2a6462127b861d2dcc1456621c25
Michael Cruz <mcruz@aersale.com>
2024-05-28 13:47:59 -0400

    image grid styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export const dynamic = "force-dynamic";
 async function Images() {
   const images = await getMyImages();
   return (
-    <div className="flex flex-wrap justify-center gap-4">
-      {images.map((image) => (
+    <div className="flex flex-wrap justify-center gap-4 p-4">
+      {[...images, ...images, ...images, ...images].map((image) => (
         <div key={image.id} className="w-48 flex-col p-2">
           <Link href={`/img/${image.id}`}>
             <Image
