a5dee6e72e34ca003a2a30eb4cadba12c9a98f1a
Michael Cruz <mcruz@aersale.com>
2024-05-28 14:07:57 -0400

    removed repeat data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ async function Images() {
   const images = await getMyImages();
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {[...images, ...images, ...images, ...images].map((image) => (
+      {images.map((image) => (
         <div key={image.id} className="w-48 flex-col p-2">
           <Link href={`/img/${image.id}`}>
             <Image
